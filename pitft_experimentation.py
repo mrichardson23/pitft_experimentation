921890888219260a3637649073066123be735d94
Matt Richardson <mrichardson23@gmail.com>
2015-07-06 23:47:13 +0000

    add color selection

--- pitft_experimentation.py
+++ pitft_experimentation.py
@@ -12,6 +12,11 @@ buttonC = 23
 buttonD = 27
 buttonPressed = None
 
+OptionAColor = (255,255,255,255)
+OptionBColor = (255,255,255,255)
+OptionCColor = (255,255,255,255)
+OptionDColor = (255,255,255,255)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buttonA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(buttonB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -35,9 +40,12 @@ while (refreshNeeded):
 	d = ImageDraw.Draw(canvas)
 
 	d.rectangle([0,0,240,320], fill=(0,0,0))
-	d.text((10,5), "Option A", font=fnt, fill=(255,255,255,255))
-	d.text((10,95), "Option B", font=fnt, fill=(255,255,255,255))
-	d.text((10,185), "Option C", font=fnt, fill=(255,255,255,255))
+
+
+
+	d.text((10,5), "Option A", font=fnt, fill=OptionAColor)
+	d.text((10,95), "Option B", font=fnt, fill=OptionBColor)
+	d.text((10,185), "Option C", font=fnt, fill=OptionCColor)
 	if (local_ip_address == "No IP"):
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -46,32 +54,51 @@ while (refreshNeeded):
 			refreshNeeded = True
 		except:
 			local_ip_address = "No IP"
-	d.text((10,275), local_ip_address, font=fnt, fill=(255,255,255,255))
+	d.text((10,275), local_ip_address, font=fnt, fill=OptionDColor)
 	canvas = canvas.rotate(90)
 	canvas.save("/tmp/test.png")
+
 	if (refreshNeeded):
 		print "Refreshing Display..."
 		os.system('sudo fbi -T 2 -d /dev/fb1 -noverbose /tmp/test.png')
 		refreshNeeded = False
-	sleep(1)
+	sleep(.3)
+	while (refreshNeeded == False):
+		if not GPIO.input(buttonA):
+			print "Button A Pressed."
+			buttonPressed = "A"
+			refreshNeeded = True
+			OptionAColor = (255,0,0,255)
+			OptionBColor = (255,255,255,255)
+			OptionCColor = (255,255,255,255)
+			OptionDColor = (255,255,255,255)
+		if not GPIO.input(buttonB):
+			print "Button B Pressed."
+			buttonPressed = "B"
+			refreshNeeded = True
+			OptionAColor = (255,255,255,255)
+			OptionBColor = (255,0,0,255)
+			OptionCColor = (255,255,255,255)
+			OptionDColor = (255,255,255,255)
+		if not GPIO.input(buttonC):
+			print "Button C Pressed."
+			buttonPressed = "C"
+			refreshNeeded = True
+			OptionAColor = (255,255,255,255)
+			OptionBColor = (255,255,255,255)
+			OptionCColor = (255,0,0,255)
+			OptionDColor = (255,255,255,255)
+		if not GPIO.input(buttonD):
+			print "Button D Pressed."
+			buttonPressed = "D"
+			refreshNeeded = True
+			OptionAColor = (255,255,255,255)
+			OptionBColor = (255,255,255,255)
+			OptionCColor = (255,255,255,255)		
+			OptionDColor = (255,0,0,255)
 
-while (refreshNeeded == False):
-	if not GPIO.input(buttonA):
-		print "Button A Pressed."
-		buttonPressed = "A"
-		refreshNeeded = True
-	if not GPIO.input(buttonB):
-		print "Button B Pressed."
-		buttonPressed = "B"
-		refreshNeeded = True
-	if not GPIO.input(buttonC):
-		print "Button C Pressed."
-		buttonPressed = "C"
-		refreshNeeded = True
-	if not GPIO.input(buttonD):
-		print "Button D Pressed."
-		buttonPressed = "D"
-		refreshNeeded = True
-	sleep(.1)
+		sleep(.1)
+
+
 
 
